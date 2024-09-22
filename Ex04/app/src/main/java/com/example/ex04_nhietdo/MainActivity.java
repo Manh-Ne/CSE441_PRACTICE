6a8964112b1409d7da9b42d71c070a84001152c6
Manh-Ne <manhtlu19@gmail.com>
2024-09-22 15:54:42 +0700

    Main Activity

--- Ex04/app/src/main/java/com/example/ex04_nhietdo/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04_nhietdo/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.ex04_nhietdo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtdoC, edtdoF;
+    Button btncf, btnfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtdoC = findViewById(R.id.txtCel);
+        edtdoF = findViewById(R.id.txtFar);
+        btncf = findViewById(R.id.btnCel);
+        btnfc = findViewById(R.id.btnFar);
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtdoC.getText()+"";
+                int C = Integer.parseInt(doC);
+                edtdoF.setText(""+dcf.format(C*1.8+32));
+            }
         });
+       
     }
 }
