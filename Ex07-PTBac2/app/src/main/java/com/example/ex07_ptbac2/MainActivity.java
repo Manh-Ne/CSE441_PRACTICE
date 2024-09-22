ce89931532695c3236874db7fba3ab2a3c5825ea
Manh-Ne <manhtlu19@gmail.com>
2024-09-23 06:38:54 +0700

    Main, Result Activity PT bac2

--- Ex07-PTBac2/app/src/main/java/com/example/ex07_ptbac2/MainActivity.java
+++ Ex07-PTBac2/app/src/main/java/com/example/ex07_ptbac2/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.ex07_ptbac2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edta, edtb;
+    Button btnkq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edta =  findViewById(R.id.txta);
+        edtb = findViewById(R.id.txtb);
+        btnkq = findViewById(R.id.btnketqua);
+        btnkq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, ResultActivity.class);
+                Bundle bundle1 = new Bundle();
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                bundle1.putInt("So A", a);
+                bundle1.putInt("So B", b);
+                myintent.putExtra("mybackage", bundle1);
+                startActivity(myintent);
+            }
         });
     }
 }
--- Ex07-PTBac2/app/src/main/java/com/example/ex07_ptbac2/ResultActivity.java
+++ Ex07-PTBac2/app/src/main/java/com/example/ex07_ptbac2/ResultActivity.java
@@ -1,24 +1,43 @@
 package com.example.ex07_ptbac2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class ResultActivity extends AppCompatActivity {
-
+    TextView edtkq;  // Sửa thành TextView thay vì EditText
+    Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtkq = findViewById(R.id.txtketqua);
+        btnback = findViewById(R.id.btnBack);
+        Intent yourIntent = getIntent();
+        Bundle yourBundle = yourIntent.getBundleExtra("mybackage");
+        int a = yourBundle.getInt("So A");
+        int b = yourBundle.getInt("So B");
+        String kq = "";
+        if (a == 0 && b != 0) {
+            kq = "Phương trình vô nghiệm";
+        } else if (a == 0 && b == 0) {
+            kq = "Phương trình có vô số nghiệm";
+        } else {
+            DecimalFormat dcf = new DecimalFormat("0.##");
+            kq = dcf.format(-b * 1.0 / a);
+        }
+        edtkq.setText(kq);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
