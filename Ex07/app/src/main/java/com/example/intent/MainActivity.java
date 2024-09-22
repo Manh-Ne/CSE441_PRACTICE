6818bef05131dae10fef19d6e703270388f3d141
Manh-Ne <manhtlu19@gmail.com>
2024-09-23 06:31:14 +0700

    Main Activity

--- Ex07/app/src/main/java/com/example/intent/MainActivity.java
+++ Ex07/app/src/main/java/com/example/intent/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn1 = findViewById(R.id.btnOpenChild);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, child_activity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
--- Ex07/app/src/main/java/com/example/intent/child_activity.java
+++ Ex07/app/src/main/java/com/example/intent/child_activity.java
@@ -1,6 +1,31 @@
 package com.example.intent;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class child_activity extends Activity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class child_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Button btn = findViewById(R.id.btnBackToMain);
+        TextView txt1 = findViewById(R.id.txtChild);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(child_activity.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
 }
