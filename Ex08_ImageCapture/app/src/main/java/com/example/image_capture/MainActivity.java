bdb00fb5aa8f33a01454f610acc25a3d7f142f6d
Manh-Ne <manhtlu19@gmail.com>
2024-09-23 10:11:43 +0700

    Image Capture Main Activity

--- Ex08_ImageCapture/app/src/main/java/com/example/image_capture/MainActivity.java
+++ Ex08_ImageCapture/app/src/main/java/com/example/image_capture/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.image_capture;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myimg = findViewById(R.id.myimg);
         btncapture = findViewById(R.id.btncapture);
+        btncapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(ACTION_IMAGE_CAPTURE);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                            Manifest.permission.CAMERA
+                    }, 1);
+                    return;
+                }
+                startActivityForResult(myintent, 99);
 
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 99 && resultCode == Activity.RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            myimg.setImageBitmap(photo);
+        }
     }
 }
