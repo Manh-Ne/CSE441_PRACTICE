abc1b13755dccfadfca7642e626bbaffaac9da30
Manh-Ne <manhtlu19@gmail.com>
2024-09-23 09:54:38 +0700

    Image Capture

--- Ex08_ImageCapture/app/src/main/java/com/example/image_capture/MainActivity.java
+++ Ex08_ImageCapture/app/src/main/java/com/example/image_capture/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.image_capture;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView myimg;
+    ImageButton btncapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        myimg = findViewById(R.id.myimg);
+        btncapture = findViewById(R.id.btncapture);
+
     }
 }
