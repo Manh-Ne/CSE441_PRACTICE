bd7820ccbb660dcd5e66d33330f9c231627f3961
Manh-Ne <manhtlu19@gmail.com>
2024-09-30 10:08:50 +0700

    MainActvity

--- Ex12/Ex12_GhiChu/app/src/main/java/com/example/ghichu/MainActivity.java
+++ Ex12/Ex12_GhiChu/app/src/main/java/com/example/ghichu/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ghichu;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,12 +11,15 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
@@ -34,5 +40,10 @@ public class MainActivity extends AppCompatActivity {
         txtdate = findViewById(R.id.txtDate);
         arraywork = new ArrayList<>();
         arrAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arraywork);
+        lv.setAdapter(arrAdapter);
+        Date currentDate = Calendar.getInstance().getTime();
+        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtdate.setText("Hôm nay: "+simpleDateFormat.format(currentDate));
+        
     }
 }
