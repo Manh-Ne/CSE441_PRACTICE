41be460c1198ba18f8829debba31c70b34423cd4
Manh-Ne <manhtlu19@gmail.com>
2024-09-30 09:55:47 +0700

    Activity_main

--- Ex12/Ex12_GhiChu/app/src/main/java/com/example/ghichu/MainActivity.java
+++ Ex12/Ex12_GhiChu/app/src/main/java/com/example/ghichu/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ghichu;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    ArrayList<String> arraywork;
+    ArrayAdapter<String> arrAdapter;
+    EditText edtwork, edth, edtm;
+    TextView txtdate;
+    Button btnwork;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        edth = findViewById(R.id.edthour);
+        edtm = findViewById(R.id.edtmi);
+        edtwork = findViewById(R.id.edtwork);
+        btnwork = findViewById(R.id.btnadd);
+        lv = findViewById(R.id.listView1);
+        txtdate = findViewById(R.id.txtDate);
+        arraywork = new ArrayList<>();
+        arrAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arraywork);
     }
 }
