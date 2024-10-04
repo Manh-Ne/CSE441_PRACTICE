ae235825c896d3d9224811a46285c025be603d3b
Manh-Ne <manhtlu19@gmail.com>
2024-10-05 06:19:25 +0700

    Add SubActivity

--- Ex13/Bai3/app/src/main/java/com/example/pj_3/MyArrayAdapter.java
+++ Ex13/Bai3/app/src/main/java/com/example/pj_3/MyArrayAdapter.java
@@ -1,7 +1,15 @@
 package com.example.pj_3;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
@@ -16,5 +24,16 @@ public class MyArrayAdapter extends ArrayAdapter<Phone> {
         this.idLayout = idLayout;
         this.myList = myList;
     }
-    
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        LayoutInflater myInflactor = context.getLayoutInflater();
+        convertView = myInflactor.inflate(idLayout,null);
+        Phone myPhone = myList.get(position);
+        ImageView imgPhone = convertView.findViewById(R.id.imgPhone);
+        imgPhone.setImageResource(myPhone.getImagePhone());
+        TextView txtNamePhone = convertView.findViewById(R.id.txtNamePhone);
+        txtNamePhone.setText(myPhone.getNamePhone());
+        return convertView;
+    }
 }
