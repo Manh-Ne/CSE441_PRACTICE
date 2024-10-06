a72638274116f6af67805396de9ab9208d9b2cb4
Manh-Ne <manhtlu19@gmail.com>
2024-10-07 06:33:07 +0700

    Xay dung list, adapter

--- Ex17/app/src/main/java/com/example/pj17/MainActivity.java
+++ Ex17/app/src/main/java/com/example/pj17/MainActivity.java
@@ -22,11 +22,31 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    String DB_PATH_SUFFIX = "/databases/";
+    SQLiteDatabase database = null;
+    String DATABASE_NAME = "QLSach.db";
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        processCopy();
+        database = openOrCreateDatabase("QLSach.db", MODE_PRIVATE, null);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+        Cursor c = database.query("tbsach", null,null,null,null,null,null);
+        c.moveToFirst();
+        String data="";
+        while (c.isAfterLast() == false){
+            data = c.getString(0)+"-"+c.getString(1)+"-"+c.getString(2);
+            mylist.add(data);
+            c.moveToNext();
+        }
+        c.close();
+        myadapter.notifyDataSetChanged();
     }
 }
