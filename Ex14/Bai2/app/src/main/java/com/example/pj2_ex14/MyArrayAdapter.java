36249909b0edee4014161c36639edd5a2fa03046
Manh-Ne <manhtlu19@gmail.com>
2024-10-06 10:31:57 +0700

    Add giao dien Bai 1 ex14

---
+++ Ex14/Bai2/app/src/main/java/com/example/pj2_ex14/MyArrayAdapter.java
@@ -0,0 +1,23 @@
+package com.example.pj2_ex14;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class MyArrayAdapter extends ArrayAdapter<Item> {
+    Activity context = null;
+    ArrayList<Item> myArray = null;
+    int LayoutId;
+
+    
+}
