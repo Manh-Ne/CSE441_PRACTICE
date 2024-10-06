adc45f81506623712658bf39f3c0a10921bb0789
Manh-Ne <manhtlu19@gmail.com>
2024-10-06 10:32:26 +0700

    Add Adapter

--- Ex14/Bai2/app/src/main/java/com/example/pj2_ex14/MyArrayAdapter.java
+++ Ex14/Bai2/app/src/main/java/com/example/pj2_ex14/MyArrayAdapter.java
@@ -19,5 +19,30 @@ public class MyArrayAdapter extends ArrayAdapter<Item> {
     ArrayList<Item> myArray = null;
     int LayoutId;
 
-    
+    public MyArrayAdapter( Activity context, ArrayList<Item> arr, int LayoutId) {
+        super(context, LayoutId, arr);
+        this.context = context;
+        this.myArray = arr;
+        this.LayoutId = LayoutId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        convertView = inflater.inflate(LayoutId, null);
+        Item myItem = myArray.get(position);
+        ImageView btnLike = convertView.findViewById(R.id.btnLike);
+        int thich = myItem.getThich();
+        if (thich==1){
+            btnLike.setImageResource(R.drawable.like);
+        } else {
+            btnLike.setImageResource(R.drawable.unlike);
+        }
+        TextView tieude = convertView.findViewById(R.id.txtTieuDe);
+        tieude.setText(myItem.getTieuDe());
+        TextView maso = convertView.findViewById(R.id.txtMaSo);
+        maso.setText(myItem.getMaSo());
+        return convertView;
+    }
 }
