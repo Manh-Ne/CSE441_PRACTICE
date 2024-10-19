ce277db1c81dfa9b5f91886c031b995e5915dd29
Manh-Ne <manhtlu19@gmail.com>
2024-10-19 08:22:25 +0700

    MainActivity

--- Ex20/app/src/main/java/com/example/pj20/MainActivity.java
+++ Ex20/app/src/main/java/com/example/pj20/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pj20;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStart;
+    MyAsyncTask mytt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doStart();
+            }
+        });
+    }
+    public void doStart(){
+        mytt = new MyAsyncTask(this);
+        mytt.execute();
     }
 }
