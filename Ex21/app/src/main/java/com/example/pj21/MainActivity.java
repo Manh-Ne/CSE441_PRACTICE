bb7837799fd076b63938f4fccefc89d7101b7591
Manh-Ne <manhtlu19@gmail.com>
2024-10-19 08:38:42 +0700

    MainActivity

--- Ex21/app/src/main/java/com/example/pj21/MainActivity.java
+++ Ex21/app/src/main/java/com/example/pj21/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pj21;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    Button btnParse;
+    ListView lv;
+    ArrayList<String> myList;
+    ArrayAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnParse = findViewById(R.id.btnParse);
+        lv = findViewById(R.id.lv1);
+        myList = new ArrayList<>();
+        myAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,myList);
+        lv.setAdapter(myAdapter);
+        btnParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
         });
     }
 }
